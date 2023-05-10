5b04cbae3faceb5eaffa24fc9d7a4992d71b8426
Eito Takase <eito01178@gmail.com>
2023-05-10 09:29:56 +0900

    revalidate

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/new-tab/:path*",
-        headers: [
-          {
-            key: "cache-control",
-            value: "public, max-age=21600, revalidate=21600",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
--- src/app/new-tab/[userName]/page.tsx
+++ src/app/new-tab/[userName]/page.tsx
@@ -2,13 +2,16 @@ import styles from "./page.module.css";
 import { Clock } from "../components/clock/clock";
 import { Contributes } from "../components/contributes";
 import { DynamicBackground } from "../components/dynamic-background";
+import { RequestContext } from "next/dist/server/base-server";
 
 export const metadata = {
   title: "New Tab",
   description: "This is New Tab Override Page",
 };
 
-export default function NewTab({ params }: { params: { userName: string } }) {
+export const revalidate = 3600;
+
+export default function NewTab({ params }: { params: { userName: string }; context: RequestContext }) {
   return (
     <div className={styles.wrapper}>
       <DynamicBackground></DynamicBackground>
