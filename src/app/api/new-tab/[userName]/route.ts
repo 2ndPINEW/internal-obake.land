61b4266df36765bde6e0151a374036c1dbdb8873
Eito Takase <eito01178@gmail.com>
2023-05-13 21:03:10 +0900

    キャッシュヘッダー制御できないからAPIルートから返したる

---
+++ src/app/api/new-tab/[userName]/route.ts
@@ -0,0 +1,16 @@
+export async function GET(request: Request) {
+  const url = new URL(request.url);
+  const path = url.pathname;
+  const appRoutePath = path.replace("/api", "");
+  const origin = url.origin;
+
+  const res = await fetch(origin + appRoutePath);
+  const body = await res.text();
+
+  return new Response(body.toString(), {
+    headers: {
+      "content-type": "text/html;charset=UTF-8",
+      "cache-control": "s-maxage=3600, stale-while-revalidate",
+    },
+  });
+}
